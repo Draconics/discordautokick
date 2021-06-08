f124b63c54aa6b73726d6785fb321c7c713532b4
Draconics <47506691+Draconics@users.noreply.github.com>
2021-06-08 13:15:30 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+toke = 'INSERT TOKEN HERE'
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+prefix = "*"
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_member_join(member):
+    case = member.name.lower()
+    if '.com/' in case:
+        await member.kick()
+
+
+client.run(token)
